5733bc6186e8ce414944f87935c917845869a39e
jrkinouani <kinouani@gmail.com>
2017-10-18 08:58:38 +0400

    add full_name user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         validates :fullname, presence: true, length: {maximum: 65}
+         validates :full_name, presence: true, length: {maximum: 65}
 end
--- db/migrate/20171014064030_addfullname_to_user.rb
+++
@@ -1,4 +0,0 @@
-class AddfullnameToUser < ActiveRecord::Migration[5.1]
-  def change
-  end
-end

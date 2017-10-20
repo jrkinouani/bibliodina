ffc43041040a0296b7a785dc7f412e4fe2d272c0
jrkinouani <kinouani@gmail.com>
2017-10-20 09:21:19 +0400

    add description table user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
                     :path_names=> {:sign_in=>"login", :sign_out=>"logout", :edit=>"profile"}
 
   root "pages#home"
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

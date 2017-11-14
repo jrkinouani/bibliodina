05a3e03eff1f8d93651d7ae12dbfc84772834b38
jrkinouani <kinouani@gmail.com>
2017-11-14 16:45:18 +0400

    preview image form book

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '',
-                    :path_names=> {:sign_in=>"login", :sign_out=>"logout", :edit=>"profile"}
+                    :path_names=> {:sign_in=>"login", :sign_out=>"logout", :edit=>"profil"}
 
 
   root "pages#home"

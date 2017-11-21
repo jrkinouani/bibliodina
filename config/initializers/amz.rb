b251a9b7cf06e6af65029cb26ed4d6e336494f75
jrkinouani <kinouani@gmail.com>
2017-11-21 17:37:41 +0400

    upload image heroku

--- config/initializers/amz.rb
+++ config/initializers/amz.rb
@@ -1,21 +1,17 @@
-  #CarrierWave.configure do |config|
-     #config.fog_provider = 'fog/aws'
-      #config.fog_credentials = {
-        #provider: 'AWS',
-        #:s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
-        #:s3_secret_access_key  => ENV['S3_SECRET_KEY'],
-        #region:                'us-east-1',
-      #  host:                  's3.amazonaws.com'
-      ##}
+  CarrierWave.configure do |config|
+     config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+        provider: 'AWS',
+        :aws_access_key_id => ENV['aws_access_key_id'],
+         :aws_secret_access_key => ENV['aws_secret_access_key'],
+         :region    =>'us-east-1',
+         :host => 's3.amazonaws.com'
 
-      #config.fog_directory  = ENV['S3_Bucket']
-      #config.fog_public     = false
-      #config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
-  #  end
+      }
 
-  ##########################################
-  # /!\ Temporary patch
-  # disable CA-certificate check on Mac OSX
-  ##########################################
-    #require 'excon'
-  #Excon.ssl_verify_peer = false
+      config.fog_directory  = "bibliodina"
+      config.fog_public     = false
+      config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+    end
+
+    

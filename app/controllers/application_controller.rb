9b6c5ff3a0e978ae160937a16406546135b325e1
jrkinouani <kinouani@gmail.com>
2018-01-20 12:43:35 +0400

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource_or_scope)
-      if resource_or_scope.is_a?(User)
-        portal_path
-      elsif resource_or_scope.is_a?(AdminUser)
-        admin_dashboard_path(resource_or_scope)
-      end
-    end
-    
+
   protected
   def  configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])

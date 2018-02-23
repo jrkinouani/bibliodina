454fcabe1adb88cb274ea7b645f554be37102bcc
jrkinouani <kinouani@gmail.com>
2018-02-23 15:48:51 +0400

    change line devise admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users, :path => '',
                     :path_names=> {:sign_in=>"login", :sign_out=>"logout", :edit=>"profil"}
 

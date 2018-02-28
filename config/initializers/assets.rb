06a33c7f9468a52c354c79c3fa7ceb63360a347a
Oualid JOUHRI <oualidjouhri@oualids-mbp.home>
2018-02-28 15:28:46 +0100

    hotfix rails admin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.initialize_on_precompile = false
-Rails.application.config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+Rails.application.config.assets.precompile += %w( rails_admin/rails_admin.css rails_admin/rails_admin.js )

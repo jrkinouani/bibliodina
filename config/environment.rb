16c3f7a2c00684be73bd7a95e33f0309aac44e33
jrkinouani <kinouani@gmail.com>
2018-03-03 15:01:57 +0400

    add actionmailer prod

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
 
  :address => 'smtp.sendgrid.net',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => 'bibliorun.herokuapp.com', :protocol => 'https' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -89,4 +87,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = {host: 'https://bibliorun.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  Si vous voulez des paramètres différents
+
 end
